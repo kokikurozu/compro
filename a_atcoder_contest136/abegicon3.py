5c287e6a6c23531cad699df60a563dd9bc5efb50
aoao <koki.kurozu@gmail.com>
2020-12-09 00:02:20 +0900

    atcoder136

--- a_atcoder_contest136/abegicon3.py
+++ a_atcoder_contest136/abegicon3.py
@@ -2,14 +2,11 @@ N = int(input())
 hi = list(map(int,input().split()))
 Is = 1
 for i in range(1,N):
-    if hi[i] - hi[i-1] >= 0:
-        pass
-    elif hi[i] - hi[i-1] == -1:
+    if hi[i] - hi[i-1] >= 1:
         hi[i] -= 1
-    else:
+    elif hi[i] - hi[i-1] < 0:
         Is = 0
         break
-    hi[N-1]
 if Is == 1:
     print('Yes')
 else:
