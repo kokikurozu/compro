97381bb7fc1e838eea27ad04758e19a21855b010
aoao <koki.kurozu@gmail.com>
2021-03-13 23:44:06 +0900

    atcoder195

---
+++ 競プロ入出力用/deepcopy.py
@@ -0,0 +1,27 @@
+from copy import deepcopy
+n = 3
+xi = [[0,1],[5,3],[6,4]]
+xi_copy = xi
+xi_copy[0][1] = 100
+print(xi_copy)
+print(xi)
+yi = [[0,1],[5,3],[6,4]]
+yi_copy = deepcopy(yi)
+yi_copy[0][1] = 100
+print(yi_copy)
+print(yi)
+zi = [[0,1],[5,3],[6,4]]
+zi_copy = [0 for i in range(n)]
+for i in range(n):
+    zi_copy[i] = zi[i]
+zi_copy[0][1] = 100
+print(zi_copy)
+print(zi)
+vi = [[0,1],[5,3],[6,4]]
+vi_copy = [[] for _ in range(n)]
+for i in range(n):
+    for j in range(2):
+        vi_copy[i].append(vi[i][j])
+vi_copy[0][1] = 100
+print(vi_copy)
+print(vi)
